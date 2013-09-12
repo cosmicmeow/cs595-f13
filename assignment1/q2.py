2061dfe83e5b53841c5ef6f570964fd5eeb313c7
Stanzheng <szhen002@odu.edu>
2013-09-12 16:26:56 -0400

    modifying

--- assignment1/q2.py
+++ assignment1/q2.py
@@ -1,10 +1,22 @@
-#Q2
-
+#Stanley Zheng
+#CS495/595 Assignment 1
+#Q2 Parse and grab scores from ESPN website
+#usage  python ./q2.py team time website
+#sources
+#using .encode('utf-8') to solve ascii errors 
+	#http://stackoverflow.com/questions/9942594/unicodeencodeerror-ascii-codec-cant-encode-character-u-xa0-in-position-20
+#Hany ~ office hours help
+	#jsoup
 from bs4 import BeautifulSoup
 import urllib2
+import sys
+import time
 
-
-school = ""
+#final system args
+#school = sys.argv[1]
+#time = sys.argv[2]
+#website = sys.argv[3]
+school = "Florida Atl"
 time = 0
 website = "http://scores.espn.go.com/ncf/scoreboard?confId=80&seasonYear=2013&seasonType=2&weekNumber=2"
 
@@ -16,8 +28,17 @@ soup = BeautifulSoup(getHtml)
 #	print scores.text
 #for teamNames in soup.html.body.find_all('a'):
 #	print teamNames.get('title')
-for teamNames in soup.html.body.find_all('div',{'class':'final-state'}):
-	print teamNames
+for teamNames in soup.html.body.find_all('div',{'class':'mod-content'}):
+	if school in teamNames.get_text():
+		print school
+	#visitors
+	 	visitor =teamNames.find_all('div',{'class':'team visitor'})
+	 	home = teamNames.find_all('div',{'class':'team home'})
+	 	#print repr(visitor)
+	 	print repr(home)
+	 	#print repr(home)
+
+	# teamNames.prettify().encode('utf-8')
 	#print teamNames.get('div',{'class':'team visitor'})
 	#print teamNames.get('div',{'class':'team home'})
 #team-home
