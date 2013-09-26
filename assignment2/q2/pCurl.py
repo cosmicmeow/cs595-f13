8f9b8ff3d6d830a7b99fbb33aa51cad6a27ac199
Stanzheng <szhen002@odu.edu>
2013-09-26 16:42:59 -0400

    checking histogram

---
+++ assignment2/q2/pCurl.py
@@ -0,0 +1,25 @@
+import pycurl
+import requests
+
+odu_memento = "http://mementoproxy.cs.odu.edu/aggr/timemap/link/"
+f = open('final_uri_list.txt', 'r')
+t = open('memento.txt', 'w')
+l = f.read().splitlines()
+#print l
+num = 0
+for each in l:
+  	try:
+  		query = odu_memento+ each
+  		#print each
+  		num = num + 1
+		r= requests.get(query)
+		if (r.status_code==200):
+			#print r.url 
+			search = r.text
+			count = [i for i in range(len(search)) if search.startswith('memento', i)]
+			t.write(each+" "+ count +"\n")
+		elif (r.status_code==404):
+			print each+" 0\n"
+			t.write(each+" 0\n")
+	except:
+		pass
